0e2bb282e8022a87fba12da9b42c0c2712c406b0
121Sreelakshmi <chennasreelakshmi9@gmail.com>
2025-07-05 13:31:33 +0530

    Update app.py

--- HOME MADE/app.py
+++ HOME MADE/app.py
@@ -4,7 +4,7 @@ import uuid
 import os
 from botocore.exceptions import ClientError
 
-app = Flask(_name_)
+app = Flask(__name__)
 app.secret_key = "9a4f90b2b6df594f2e16f6c1f3d9e0ab0cd431c0f0176a2544e740c94cb75a0e"
 
 # --------------------------------------------------
